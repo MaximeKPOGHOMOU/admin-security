24d06462ebe685c58bd6bb0bf272a15bc1f27fc3
Maxime KPOGHOMOU <maximekpoghomou18@gmail.com>
2026-03-12 10:36:50 +0000

    first commit

--- src/app/dialog/agent-dialog/agent-dialog.ts
+++ src/app/dialog/agent-dialog/agent-dialog.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatFormField, MatInputModule, MatLabel } from "@angular/material/input";
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-agent-dialog',
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatSelectModule],
   templateUrl: './agent-dialog.html',
   styleUrl: './agent-dialog.css',
 })
 export class AgentDialog {
+   constructor(private dialogRef: MatDialogRef<AgentDialog>) { }
+    closeDialog() {
+    this.dialogRef.close(); // ferme la boîte de dialogue
+  }
+  selectedFileName = '';
+
+onFileSelected(event: any) {
+  const file: File = event.target.files[0];
+  if (file) {
+    this.selectedFileName = file.name;
+    console.log(file);
+    // ici tu peux envoyer le fichier à ton serveur ou le stocker
+  }
+}
 
 }
