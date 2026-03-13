56c872f9571a98fb3fb4ac320ff71ef726c20d26
Maxime KPOGHOMOU <maximekpoghomou18@gmail.com>
2026-03-13 17:34:02 +0000

    ajout de la page login

--- src/app/dialog/agent-dialog/agent-dialog.ts
+++ src/app/dialog/agent-dialog/agent-dialog.ts
@@ -1,29 +1,41 @@
 import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatFormField, MatInputModule, MatLabel } from "@angular/material/input";
+import { MatInputModule } from "@angular/material/input";
 import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-agent-dialog',
-  imports: [MatFormFieldModule, MatInputModule, MatSelectModule],
   templateUrl: './agent-dialog.html',
-  styleUrl: './agent-dialog.css',
+  styleUrls: ['./agent-dialog.css'],
+  imports: [MatFormFieldModule, MatInputModule, MatSelectModule] // si standalone
 })
 export class AgentDialog {
-   constructor(private dialogRef: MatDialogRef<AgentDialog>) { }
-    closeDialog() {
+  constructor(private dialogRef: MatDialogRef<AgentDialog>) { }
+
+  closeDialog() {
     this.dialogRef.close(); // ferme la boîte de dialogue
   }
-  selectedFileName = '';
 
-onFileSelected(event: any) {
-  const file: File = event.target.files[0];
-  if (file) {
-    this.selectedFileName = file.name;
-    console.log(file);
-    // ici tu peux envoyer le fichier à ton serveur ou le stocker
+  // Upload photo
+  selectedPhotoName = '';
+  onPhotoSelected(event: any) {
+    const file: File = event.target.files[0];
+    if (file) {
+      this.selectedPhotoName = file.name;
+      console.log(file);
+      // envoyer le fichier au serveur si besoin
+    }
   }
-}
 
+  // Upload document
+  selectedPieceName = '';
+  onPieceSelected(event: any) {
+    const file: File = event.target.files[0];
+    if (file) {
+      this.selectedPieceName = file.name;  // corrige ici
+      console.log(file);
+      // envoyer le fichier au serveur si besoin
+    }
+  }
 }
--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -5,12 +5,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatCardModule } from '@angular/material/card';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
 
 
 
 @Component({
   selector: 'app-login',
-  imports: [MatButtonModule, MatInputModule, MatCheckboxModule, MatCardModule],
+  imports: [MatButtonModule, MatInputModule, MatCheckboxModule, MatCardModule, MatIconModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
